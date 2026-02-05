96a68e1c398f01d35454844bd052669969b11877
66010914066-arch <66010914066@msu.ac.th>
2026-02-05 22:42:40 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,14 +1,7 @@
 <?php
-$host = "localhost";
-$user = "4066db";
-$pwd = "4066db";
-$db = "4066db";
-
-$conn = mysqli_connect($host, $user, $pwd, $db);
-
-if (!$conn) {
-    die("เชื่อมต่อไม่ได้ : " . mysqli_connect_error());
-}
-
-mysqli_set_charset($conn, "utf8mb4");
+	$host = "localhost";
+    $user = "root";
+    $pwd = "";
+    $db = "4066db";
+   $conn = mysqli_connect($host, $user, $pwd, $db) or die(mysqli_connect_error());
 ?>
