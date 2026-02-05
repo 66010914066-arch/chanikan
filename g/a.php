9f8b48b7590e4e6dc9b16508f304ac59dff5a24e
66010914066-arch <66010914066@msu.ac.th>
2026-02-05 21:24:51 +0700

    Refactor HTML and PHP for better structure

--- g/a.php
+++ g/a.php
@@ -1,57 +1,32 @@
-<!doctype html>
+<?php
+$host = "localhost";
+$user = "chanikandb"; 
+$pwd = "123456"; 
+$db = "4066db";
+$conn = mysqli_connect($host, $user, $pwd, $db);
+if (!$conn) { die("เชื่อมต่อไม่ได้: " . mysqli_connect_error()); }
+mysqli_query($conn, "SET NAMES utf8");
+$sql = "SELECT * FROM popsupermarket";
+$query = mysqli_query($conn, $sql);
+?>
 <html>
-<head>
-<meta charset="utf-8">
-<title>ชนิกานต์ ขำสระน้อย (ปุ้ม)</title>
-</head>
-
+<head><meta charset="UTF-8"></head>
 <body>
-<h1>ชนิกานต์ ขำสระน้อย (ปุ้ม)</h1>
-
+<h2>ชนิกันต์ ขำสระน้อย (ปุ๋ม)</h2>
 <table border="1">
 <tr>
-    <th>Oder ID</th>
-    <th>สินค้า</th>
-    <th>ประเภทสินค้า</th>
-    <th>วันที่</th>
-    <th>ประเทศ</th>
-    <th>จำนวนเงิน</th>
-    <th>รูป</th>
+<th>Oder ID</th><th>สินค้า</th><th>ประเภทสินค้า</th><th>วันที่</th><th>ประเทศ</th><th>จำนวนเงิน</th>
 </tr>
-
-<?php
-include_once("connectdb.php");
-$sql = "SELECT * FROM popsupermarket
-        WHERE p_country = 'Sweden'
-        AND p_category ='Vegetables'
-        ORDER BY p_product_name ASC";
-
-$rs = mysqli_query($conn,$sql);
-$total = 0;
-
-while ($data = mysqli_fetch_array($rs)){
-    $total += $data['p_amount'];
-?>
+<?php while($row = mysqli_fetch_array($query)) { ?>
 <tr>
-    <td><?php echo $data['p_order_id']; ?></td>
-    <td><?php echo $data['p_product_name']; ?></td>
-    <td><?php echo $data['p_category']; ?></td>
-    <td><?php echo $data['p_date']; ?></td>
-    <td><?php echo $data['p_country']; ?></td>
-    <td align="right"><?php echo number_format($data['p_amount'],0); ?></td>
-    <td align="center">
-        <img src="<?php echo $data['p_product_name']; ?>.jpg" width="55">
-    </td>
+<td><?php echo $row['p_order_id']; ?></td>
+<td><?php echo $row['p_product_name']; ?></td>
+<td><?php echo $row['p_category']; ?></td>
+<td><?php echo $row['p_date']; ?></td>
+<td><?php echo $row['p_country']; ?></td>
+<td><?php echo number_format($row['p_amount']); ?></td>
 </tr>
 <?php } ?>
-
-<tr>
-    <td colspan="5" align="right"><b>รวม</b></td>
-    <td align="right"><b><?php echo number_format($total,0); ?></b></td>
-    <td>&nbsp;</td>
-</tr>
-
 </table>
-
 </body>
 </html>
