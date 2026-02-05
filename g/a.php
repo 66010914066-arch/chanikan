150ca552cb56ba1dc8e522dd8de57cbb00a7773c
66010914066-arch <66010914066@msu.ac.th>
2026-02-05 21:31:30 +0700

    a

--- g/a.php
+++ g/a.php
@@ -1,60 +1,57 @@
-<?php
-$host = "localhost";
-$user = "chanikandb"; // ตรวจสอบชื่อ User บน Server อีกครั้ง
-$pwd = "123456";      // ตรวจสอบรหัสผ่านบน Server อีกครั้ง
-$db = "4066db";
-
-$conn = mysqli_connect($host, $user, $pwd, $db);
-
-if (!$conn) {
-    die("เชื่อมต่อฐานข้อมูลไม่ได้: " . mysqli_connect_error());
-}
-
-mysqli_query($conn, "SET NAMES utf8");
-$sql = "SELECT * FROM popsupermarket";
-$query = mysqli_query($conn, $sql);
-?>
-
+<!doctype html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>รายงานข้อมูลสินค้า</title>
-    <style>
-        table { border-collapse: collapse; width: 100%; }
-        th, td { border: 1px solid #000; padding: 8px; text-align: center; }
-        th { background-color: #f2f2f2; }
-        img { border-radius: 5px; object-fit: cover; }
-    </style>
+<meta charset="utf-8">
+<title>ชนิกานต์ ขำสระน้อย (ปุ้ม)</title>
 </head>
+
 <body>
-    <h2>ชนิกานต์ ขำสระน้อย (ปุ้ม)</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>Oder ID</th>
-                <th>สินค้า</th>
-                <th>ประเภทสินค้า</th>
-                <th>วันที่</th>
-                <th>ประเทศ</th>
-                <th>จำนวนเงิน</th>
-                <th>รูป</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($row = mysqli_fetch_array($query)) { ?>
-            <tr>
-                <td><?php echo $row['p_order_id']; ?></td>
-                <td><?php echo $row['p_product_name']; ?></td>
-                <td><?php echo $row['p_category']; ?></td>
-                <td><?php echo $row['p_date']; ?></td>
-                <td><?php echo $row['p_country']; ?></td>
-                <td><?php echo number_format($row['p_amount']); ?></td>
-                <td>
-                    <img src="img/<?php echo $row['p_order_id']; ?>.jpg" width="50" height="50" alt="no image">
-                </td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+<h1>ชนิกานต์ ขำสระน้อย (ปุ้ม)</h1>
+
+<table border="1">
+<tr>
+    <th>Oder ID</th>
+    <th>สินค้า</th>
+    <th>ประเภทสินค้า</th>
+    <th>วันที่</th>
+    <th>ประเทศ</th>
+    <th>จำนวนเงิน</th>
+    <th>รูป</th>
+</tr>
+
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM popsupermarket
+        WHERE p_country = 'Sweden'
+        AND p_category ='Vegetables'
+        ORDER BY p_product_name ASC";
+
+$rs = mysqli_query($conn,$sql);
+$total = 0;
+
+while ($data = mysqli_fetch_array($rs)){
+    $total += $data['p_amount'];
+?>
+<tr>
+    <td><?php echo $data['p_order_id']; ?></td>
+    <td><?php echo $data['p_product_name']; ?></td>
+    <td><?php echo $data['p_category']; ?></td>
+    <td><?php echo $data['p_date']; ?></td>
+    <td><?php echo $data['p_country']; ?></td>
+    <td align="right"><?php echo number_format($data['p_amount'],0); ?></td>
+    <td align="center">
+        <img src="<?php echo $data['p_product_name']; ?>.jpg" width="55">
+    </td>
+</tr>
+<?php } ?>
+
+<tr>
+    <td colspan="5" align="right"><b>รวม</b></td>
+    <td align="right"><b><?php echo number_format($total,0); ?></b></td>
+    <td>&nbsp;</td>
+</tr>
+
+</table>
+
 </body>
 </html>
